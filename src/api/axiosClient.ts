1b8be640baef60716a3af94f8649fae39fe76436
Gia Kinh <giakinh2000@gmail.com>
2021-12-22 19:32:43 +0700

    url

--- src/api/axiosClient.ts
+++ src/api/axiosClient.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-const baseURL = process.env.REACT_API_URL ?? "http://127.0.0.1:8000";
+const baseURL = "https://hagiakinh-api.herokuapp.com";
 
 
 const axiosClient = axios.create({
